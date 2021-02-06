2f906060589719f44863415be54e7c1b250f9f5c
Jack Schneiderhan <jschnei4@stevens.edu>
2021-02-06 01:46:34 -0500

    Create DataTypes.cpp

---
+++ DataTypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include<bits/stdc++.h>
+using namespace std;
+//Program to assist in the understanding of data types.
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << fixed << setprecision(3) << d << endl;
+    cout << fixed << setprecision(9) << e << endl;
+    return 0;
+}
